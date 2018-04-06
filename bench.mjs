09a717f276541d75d2cf59bd4657afae7098dbda
Vladimir Agafonkin <agafonkin@gmail.com>
2018-04-06 18:21:54 +0300

    first commit

---
+++ bench.mjs
@@ -0,0 +1,45 @@
+
+import FlatQueue from './';
+import TinyQueue from 'tinyqueue';
+
+const N = 1000000;
+const K = 1000;
+
+const data = [];
+for (let i = 0; i < N; i++) data[i] = {value: Math.random()};
+
+
+const q = new TinyQueue(null, (a, b) => a.value - b.value);
+
+console.time('tinyqueue push ' + N);
+for (let i = 0; i < N; i++) q.push(data[i]);
+console.timeEnd('tinyqueue push ' + N);
+
+console.time('tinyqueue pop ' + N);
+for (let i = 0; i < N; i++) q.pop();
+console.timeEnd('tinyqueue pop ' + N);
+
+console.time('tinyqueue push/pop ' + N);
+for (let i = 0; i < N; i += K) {
+    for (let j = 0; j < K; j++) q.push(data[i + j]);
+    for (let j = 0; j < K; j++) q.pop();
+}
+console.timeEnd('tinyqueue push/pop ' + N);
+
+
+const f = new FlatQueue();
+
+console.time('flatqueue push ' + N);
+for (let i = 0; i < N; i++) f.push(i, data[i].value);
+console.timeEnd('flatqueue push ' + N);
+
+console.time('flatqueue pop ' + N);
+for (let i = 0; i < N; i++) f.pop();
+console.timeEnd('flatqueue pop ' + N);
+
+console.time('flat push/pop ' + N);
+for (let i = 0; i < N; i += K) {
+    for (let j = 0; j < K; j++) f.push(i, data[i + j].value);
+    for (let j = 0; j < K; j++) f.pop();
+}
+console.timeEnd('flat push/pop ' + N);
---
+++ index.mjs
@@ -0,0 +1,86 @@
+
+export default class FlatQueue {
+
+    constructor() {
+        this.ids = [];
+        this.values = [];
+        this.length = 0;
+    }
+
+    push(id, value) {
+        this.ids.push(id);
+        this.values.push(value);
+        this.length++;
+        this._up(this.length - 1);
+    }
+
+    pop() {
+        if (this.length === 0) return undefined;
+
+        const top = this.ids[0];
+        this.length--;
+
+        if (this.length > 0) {
+            this.ids[0] = this.ids[this.length];
+            this.values[0] = this.values[this.length];
+            this._down(0);
+        }
+        this.ids.pop();
+        this.values.pop();
+
+        return top;
+    }
+
+    peek() {
+        return this.ids[0];
+    }
+
+    peekValue() {
+        return this.values[0];
+    }
+
+    _up(pos) {
+        const index = this.ids[pos];
+        const value = this.values[pos];
+
+        while (pos > 0) {
+            const parent = (pos - 1) >> 1;
+            const parentValue = this.values[parent];
+            if (value >= parentValue) break;
+            this.ids[pos] = this.ids[parent];
+            this.values[pos] = parentValue;
+            pos = parent;
+        }
+
+        this.ids[pos] = index;
+        this.values[pos] = value;
+    }
+
+    _down(pos) {
+        const halfLength = this.length >> 1;
+        const index = this.ids[pos];
+        const value = this.values[pos];
+
+        while (pos < halfLength) {
+            let left = (pos << 1) + 1;
+            const right = left + 1;
+            let bestIndex = this.ids[left];
+            let bestValue = this.values[left];
+            const rightValue = this.values[right];
+
+            if (right < this.length && rightValue < bestValue) {
+                left = right;
+                bestIndex = this.ids[right];
+                bestValue = rightValue;
+            }
+            if (bestValue >= value) break;
+
+            this.ids[pos] = bestIndex;
+            this.values[pos] = bestValue;
+            pos = left;
+        }
+
+        this.ids[pos] = index;
+        this.values[pos] = value;
+    }
+}
