18249a26b5d43b9aae452e0660ec3479db0a823b
Vladimir Agafonkin <agafonkin@gmail.com>
2018-10-10 10:14:25 +0300

    add basic tests

--- bench.mjs
+++ bench.mjs
@@ -11,35 +11,35 @@ for (let i = 0; i < N; i++) data[i] = {value: Math.random()};
 
 const q = new TinyQueue(null, (a, b) => a.value - b.value);
 
-console.time('tinyqueue push ' + N);
+console.time(`tinyqueue push ${N}`);
 for (let i = 0; i < N; i++) q.push(data[i]);
-console.timeEnd('tinyqueue push ' + N);
+console.timeEnd(`tinyqueue push ${N}`);
 
-console.time('tinyqueue pop ' + N);
+console.time(`tinyqueue pop ${N}`);
 for (let i = 0; i < N; i++) q.pop();
-console.timeEnd('tinyqueue pop ' + N);
+console.timeEnd(`tinyqueue pop ${N}`);
 
-console.time('tinyqueue push/pop ' + N);
+console.time(`tinyqueue push/pop ${N}`);
 for (let i = 0; i < N; i += K) {
     for (let j = 0; j < K; j++) q.push(data[i + j]);
     for (let j = 0; j < K; j++) q.pop();
 }
-console.timeEnd('tinyqueue push/pop ' + N);
+console.timeEnd(`tinyqueue push/pop ${N}`);
 
 
 const f = new FlatQueue();
 
-console.time('flatqueue push ' + N);
+console.time(`flatqueue push ${N}`);
 for (let i = 0; i < N; i++) f.push(i, data[i].value);
-console.timeEnd('flatqueue push ' + N);
+console.timeEnd(`flatqueue push ${N}`);
 
-console.time('flatqueue pop ' + N);
+console.time(`flatqueue pop ${N}`);
 for (let i = 0; i < N; i++) f.pop();
-console.timeEnd('flatqueue pop ' + N);
+console.timeEnd(`flatqueue pop ${N}`);
 
-console.time('flat push/pop ' + N);
+console.time(`flat push/pop ${N}`);
 for (let i = 0; i < N; i += K) {
     for (let j = 0; j < K; j++) f.push(i, data[i + j].value);
     for (let j = 0; j < K; j++) f.pop();
 }
-console.timeEnd('flat push/pop ' + N);
+console.timeEnd(`flat push/pop ${N}`);
