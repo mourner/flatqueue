00e2d629f6b904281711193889bc40731795029b
Vladimir Agafonkin <agafonkin@gmail.com>
2018-10-10 10:18:00 +0300

    inline up/down for smaller code

--- index.mjs
+++ index.mjs
@@ -14,8 +14,19 @@ export default class FlatQueue {
     push(id, value) {
         this.ids.push(id);
         this.values.push(value);
-        this.length++;
-        this._up(this.length - 1);
+
+        let pos = this.length++;
+        while (pos > 0) {
+            const parent = (pos - 1) >> 1;
+            const parentValue = this.values[parent];
+            if (value >= parentValue) break;
+            this.ids[pos] = this.ids[parent];
+            this.values[pos] = parentValue;
+            pos = parent;
+        }
+
+        this.ids[pos] = id;
+        this.values[pos] = value;
     }
 
     pop() {
@@ -25,10 +36,34 @@ export default class FlatQueue {
         this.length--;
 
         if (this.length > 0) {
-            this.ids[0] = this.ids[this.length];
-            this.values[0] = this.values[this.length];
-            this._down(0);
+            const id = this.ids[0] = this.ids[this.length];
+            const value = this.values[0] = this.values[this.length];
+            const halfLength = this.length >> 1;
+            let pos = 0;
+
+            while (pos < halfLength) {
+                let left = (pos << 1) + 1;
+                const right = left + 1;
+                let bestIndex = this.ids[left];
+                let bestValue = this.values[left];
+                const rightValue = this.values[right];
+
+                if (right < this.length && rightValue < bestValue) {
+                    left = right;
+                    bestIndex = this.ids[right];
+                    bestValue = rightValue;
+                }
+                if (bestValue >= value) break;
+
+                this.ids[pos] = bestIndex;
+                this.values[pos] = bestValue;
+                pos = left;
+            }
+
+            this.ids[pos] = id;
+            this.values[pos] = value;
         }
+
         this.ids.pop();
         this.values.pop();
 
@@ -41,50 +76,5 @@ export default class FlatQueue {
 
     peekValue() {
         return this.values[0];
-    }
-
-    _up(pos) {
-        const index = this.ids[pos];
-        const value = this.values[pos];
-
-        while (pos > 0) {
-            const parent = (pos - 1) >> 1;
-            const parentValue = this.values[parent];
-            if (value >= parentValue) break;
-            this.ids[pos] = this.ids[parent];
-            this.values[pos] = parentValue;
-            pos = parent;
-        }
-
-        this.ids[pos] = index;
-        this.values[pos] = value;
-    }
-
-    _down(pos) {
-        const halfLength = this.length >> 1;
-        const index = this.ids[pos];
-        const value = this.values[pos];
-
-        while (pos < halfLength) {
-            let left = (pos << 1) + 1;
-            const right = left + 1;
-            let bestIndex = this.ids[left];
-            let bestValue = this.values[left];
-            const rightValue = this.values[right];
-
-            if (right < this.length && rightValue < bestValue) {
-                left = right;
-                bestIndex = this.ids[right];
-                bestValue = rightValue;
-            }
-            if (bestValue >= value) break;
-
-            this.ids[pos] = bestIndex;
-            this.values[pos] = bestValue;
-            pos = left;
-        }
-
-        this.ids[pos] = index;
-        this.values[pos] = value;
     }
 }
