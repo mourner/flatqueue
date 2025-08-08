b145e245303b10ee39f9494c880a4a72e536703a
Vladimir Agafonkin <agafonkin@gmail.com>
2025-08-08 23:14:13 +0300

    microoptimizations (up to 20% faster in practical applications)

--- index.js
+++ index.js
@@ -30,36 +30,29 @@ export default class FlatQueue {
     pop() {
         if (this.length === 0) return undefined;
 
-        const top = this.ids[0];
-        this.length--;
+        const ids = this.ids,
+            values = this.values,
+            top = ids[0],
+            last = --this.length;
 
-        if (this.length > 0) {
-            const id = this.ids[0] = this.ids[this.length];
-            const value = this.values[0] = this.values[this.length];
-            const halfLength = this.length >> 1;
+        if (last > 0) {
+            const id = ids[last];
+            const value = values[last];
             let pos = 0;
+            const halfLen = last >> 1;
 
-            while (pos < halfLength) {
-                let left = (pos << 1) + 1;
+            while (pos < halfLen) {
+                const left = (pos << 1) + 1;
                 const right = left + 1;
-                let bestIndex = this.ids[left];
-                let bestValue = this.values[left];
-                const rightValue = this.values[right];
-
-                if (right < this.length && rightValue < bestValue) {
-                    left = right;
-                    bestIndex = this.ids[right];
-                    bestValue = rightValue;
-                }
-                if (bestValue >= value) break;
-
-                this.ids[pos] = bestIndex;
-                this.values[pos] = bestValue;
-                pos = left;
+                const child = left + (+(right < last) & +(values[right] < values[left]));
+                if (values[child] >= value) break;
+                ids[pos] = ids[child];
+                values[pos] = values[child];
+                pos = child;
             }
 
-            this.ids[pos] = id;
-            this.values[pos] = value;
+            ids[pos] = id;
+            values[pos] = value;
         }
 
         return top;
